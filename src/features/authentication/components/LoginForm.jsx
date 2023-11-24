9c56d20dcdad0a88a82ddc79cb617bc74b0986b9
Linda Sandaker <frk.sandkake@gmail.com>
2023-11-24 16:13:06 +0100

    forms init and connected

---
+++ src/features/authentication/components/LoginForm.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Link } from "react-router-dom"
+
+export function LoginForm() {
+  return (
+    <>
+      <button>
+        <Link to='/signup'>Take me to Sign up</Link>
+      </button>
+    </>
+  )
+}
---
+++ src/features/authentication/components/SignupForm.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Link } from "react-router-dom"
+
+export function SignupForm() {
+  return (
+    <>
+      <button>
+        <Link to='/login'>Take me to Login</Link>
+      </button>
+    </>
+  )
+}
